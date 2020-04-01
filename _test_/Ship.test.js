00b2fb00d2fdb36b54bb3888b917142bdd1e39fe
aar.56aint <aaron.hays_7@yahoo.com>
2020-04-01 14:29:27 +0100

    attempt on getPassenger

--- _test_/Ship.test.js
+++ _test_/Ship.test.js
@@ -8,9 +8,20 @@ describe('Ship function/class', () => {
 
   it('constructor Ship returns an object', () => {
     expect(new Ship).toBeInstanceOf(Object);
-  })
+  });
   //const ship = new Ship('ship');
   it('that the ship has a starting point', () => {
     expect(ship.startingPoint).toBe('Hello, welcome to Dover! Your cruise starts here (˃̣̣̥ w ˂̣̣̥)')
+  });
+
+  it('the ship can get passengers', () => {
+    expect(ship.getPassenger(1)).toBe('Hop-on friend!')
+  });
+  xit('the ship can get passengers', () => {
+    expect(ship.getPassenger(399)).toBe('Hey friend, seats are full, can you stand?')
+  });
+
+  xit('the ship can get passengers', () => {
+    expect(ship.getPassenger(601)).toBe('sorry, ship is full')
   });
 });
